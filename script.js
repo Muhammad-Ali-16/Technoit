3dc662bace3d5612ce5cb441aec1fcce2676b5ca
Muhammad Ali <149061382+Muhammad-Ali-16@users.noreply.github.com>
2024-06-26 20:58:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,14 +20,52 @@ function scrollFunction() {
     }
 }
 
-let theme_btn = document.getElementById("theme-btn");
-let theme_icon = document.getElementById("theme-icon");
+function ThemeChanger() {
 
-theme_btn.addEventListener("click", function () {
-    document.body.classList.toggle('dark-theme');
+    let theme_btn = document.getElementById("theme-btn");
+    let theme_icon = document.getElementById("theme-icon");
+    let theme_overlay_btn = document.getElementById("theme-overlay-btn");
+    let theme_overlay_icon = document.getElementById("theme-overlay-icon");
 
-    theme_icon.classList.toggle("bi-moon");
-    theme_icon.classList.toggle("bi-brightness-high-fill");
-});
+    theme_btn.addEventListener("click", function () {
+        document.body.classList.toggle('dark-theme');
+        theme_icon.classList.toggle("bi-moon");
+        theme_icon.classList.toggle("bi-brightness-high-fill");
+    });
 
+    theme_overlay_btn.addEventListener("click", function () {
+        document.body.classList.toggle('dark-theme');
+        theme_overlay_icon.classList.toggle("bi-moon");
+        theme_overlay_icon.classList.toggle("bi-brightness-high-fill");
+    });
+}
 
+ThemeChanger();
+
+function NavOverlay() {
+
+    let overlay = document.getElementById("overlay");
+    let overlay_content = document.getElementById("overlay-content");
+    let open = document.getElementById("open-btn");
+    let close = document.getElementById("close-btn");
+
+    console.log(overlay_content);
+
+    open.addEventListener("click", function openNav() {
+        overlay.style.width = "250px";
+        overlay_content.style.display = "block";
+        open.style.display = "none";
+        close.style.display = "block";
+    });
+
+    close.addEventListener("click", function closeNav() {
+        overlay.style.width = "0px";
+        overlay_content.style.display = "none";
+        open.style.display = "block";
+        close.style.display = "none";
+
+    })
+
+}
+
+NavOverlay();
