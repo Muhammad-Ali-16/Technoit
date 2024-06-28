59bc5cc9620f7a5d593cb362c86d53c4c3f5c45b
Muhammad Ali <149061382+Muhammad-Ali-16@users.noreply.github.com>
2024-06-28 16:16:39 +0500

    Added Portfolio Section

--- script.js
+++ script.js
@@ -69,3 +69,25 @@ function NavOverlay() {
 }
 
 NavOverlay();
+
+
+function openTab(evt, cityName) {
+
+    var i, gallery_content, gallery_links;
+
+    gallery_content = document.getElementsByClassName("gallery-content");
+    for (const content of gallery_content) {
+        content.style.display = "none";
+    }
+
+    gallery_links = document.getElementsByClassName("gallery-links");
+    for (const links of gallery_links) {
+        links.className = links.className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
